93c7d85972c8fe849db22e47f640124c8f43de30
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-07-24 16:18:24 +1200

    init code

--- verification/tests.py
+++ verification/tests.py
@@ -7,19 +7,23 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
+            "input": ('d3', {'d6', 'b6', 'c8', 'g4', 'b8', 'g6'}),
             "answer": 5,
-            "explanation": "3+2=?"
+            "explanation": ('d6', 'g6', 'b6', 'b8', 'c8')
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": ('a2', {'f6', 'f2', 'a6', 'f8', 'h8', 'h6'}),
+            "answer": 6,
+            "explanation": ('a6', 'f6', 'h6', 'h8', 'f8', 'f2')
+        },
+        {
+            "input": ('a2', {'f6', 'f8', 'f2', 'a6', 'h6'}),
+            "answer": 4,
+            "explanation": ('a6', 'f6', 'f2', 'f8')
+        },
     ],
     "Extra": [
         {
